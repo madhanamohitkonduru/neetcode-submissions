66868eef8c39f8d7a266fa80398d92fe53c1c8b7
Madhana Mohit Konduru <125701957+madhanamohitkonduru@users.noreply.github.com>
2026-06-15 23:02:54 -0500

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hash = {}
+        for i in nums:
+            hash[i] = hash.get(i,0)+1
+            if hash[i]==2:
+                return True
+
+        return False
+        
