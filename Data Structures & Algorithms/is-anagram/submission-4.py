77d23ba5511e3becbeb9eb205408189cc04066bd
Madhana Mohit Konduru <125701957+madhanamohitkonduru@users.noreply.github.com>
2026-06-15 23:15:13 -0500

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hash1= {}
+        hash2={}
+        len_s = len(s)
+        len_t = len(t)
+        if len_s!=len_t:
+            return False
+
+        for i in range(len_s):
+            hash1[s[i]] = hash1.get(s[i], 0) + 1
+            hash2[t[i]] = hash2.get(t[i], 0) + 1
+
+        if hash1==hash2:
+            return True
+        else:
+            return False        
