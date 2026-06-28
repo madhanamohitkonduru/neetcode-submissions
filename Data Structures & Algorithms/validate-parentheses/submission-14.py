d1e4e3ef63beb7f8137a78328dab7185ac7ea0e5
Madhana Mohit Konduru <125701957+madhanamohitkonduru@users.noreply.github.com>
2026-06-28 16:54:53 -0500

    Add: validate-parentheses - submission-14

---
+++ Data Structures & Algorithms/validate-parentheses/submission-14.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hashmap = {
+            "}" : "{",
+            "]" : "[",
+            ")" : "("
+        }
+        if len(s)==1:
+            return False
+        lis = []
+        for i in s:
+            if i in hashmap.keys():
+                if not lis or lis.pop() != hashmap[i]:
+                    return False
+            else:
+                lis.append(i)
+
+        return True if not lis else False
