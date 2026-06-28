1d509cadfd7994fc23cc3019e0abc84fc2e5993e
Madhana Mohit Konduru <125701957+madhanamohitkonduru@users.noreply.github.com>
2026-06-28 16:44:35 -0500

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hashmap = {
+            "}" : "{",
+            "]" : "[",
+            ")" : "("
+        }
+        if len(s)==1:
+            return False
+        lis = []
+        for i in s:
+            if i in hashmap.keys():
+                if len(lis)==0:
+                    return False
+                if lis.pop() != hashmap[i]:
+                    return False
+            else:
+                lis.append(i)
+
+        if len(lis)==0:
+            return True
+        else:
+            return False
