f51996349d2a4cb6202d23cf8e093d4810364c43
Madhana Mohit Konduru <125701957+madhanamohitkonduru@users.noreply.github.com>
2026-09-13 12:58:36 -0500

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<=2:
+            return n
+        lis = [0, 1, 2]
+        for i in range(3,n+1):
+            lis.append(lis[i-1]+lis[i-2])
+        return lis.pop()
